640ac05acc5a5cc54c4fa41ba929076e8c65aee4
Neha-meharin <nehamehrin09@gmail.com>
2025-07-03 23:14:27 +0530

    Create task_04.py

---
+++ Day-01/task_04.py
@@ -0,0 +1,8 @@
+name=input("enter your name")
+age=input("enter your age")
+hobby=input("enter your hobby")
+p=f"hey ! am {name},a {age} year od who loves {hobby}.Now tell me about yourself."
+print(p)
+print(p.title())
+print(p.upper())
+print(p.lower())
