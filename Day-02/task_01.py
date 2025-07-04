6dddf996290e1e0469b950aee0e7cfd801dce354
Neha-meharin <nehamehrin09@gmail.com>
2025-07-04 22:12:38 +0530

    Create task_01.py

---
+++ Day-02/task_01.py
@@ -0,0 +1,8 @@
+number=int(input("enter a num greater than zero or lesser than zero :"))
+if number>0:
+    print("number is positive")
+elif number<0:
+    print("number is negative")
+else:
+    print("you have choosen zero")
+    
