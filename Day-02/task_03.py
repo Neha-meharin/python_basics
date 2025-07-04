b4410822e17037fbcd81d054e5958aa880e76384
Neha-meharin <nehamehrin09@gmail.com>
2025-07-04 22:22:00 +0530

    Create task_03.py

---
+++ Day-02/task_03.py
@@ -0,0 +1,7 @@
+number1=int(input("enter number"))
+operator=input("enter operator")
+number2=int(input("enter second number"))
+if operator=="+":
+    print(number1+number2)
+elif operator=="-":
+    print(number1-number2)
