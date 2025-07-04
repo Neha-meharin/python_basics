9d9a4d5a6ae57d23e19e1cae99da74d3eebae976
Neha-meharin <nehamehrin09@gmail.com>
2025-07-04 22:44:09 +0530

    Create task_04.py

---
+++ Day-02/task_04.py
@@ -0,0 +1,8 @@
+ans1=input("You regularly make new friends(yes or no):")
+ans2=input("How often do you feel anxious?(always or never):")
+ans3=input("if you could have superpower what would it be(flying or invisible):")
+if ans1=="yes" and ans2=="never" and ans3=="flying":
+    print("you have extrovert mind with emotional stability")
+elif ans1=="no" and ans2=="always" and ans3=="invisible":
+    print("you have introvert mind with no emotional stability")
+    
