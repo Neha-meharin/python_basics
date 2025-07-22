7bb066d73f92342e4728d31011930627d08d53ec
Neha-meharin <nehamehrin09@gmail.com>
2025-07-22 21:05:22 +0530

    Create task_15.py

---
+++ Day6/task_15.py
@@ -0,0 +1,6 @@
+name=input("enter first string")
+name1=input("enter first string")
+f=name[0]+name1[0]
+l=name[len(name)-1]+name1[len(name1)-1]
+m=name[int(len(name)/2)]+name1[int(len(name1)/2)]
+print(f+l+m)
