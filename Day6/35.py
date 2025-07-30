90c0f8feea80295f6f3225807f5c60f00a181124
Neha-meharin <nehamehrin09@gmail.com>
2025-07-30 21:48:11 +0530

    Create 35.py

---
+++ Day6/35.py
@@ -0,0 +1,13 @@
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+x= sampleDict["class"]["student"]["name"]="jessa"
+print(sampleDict)
