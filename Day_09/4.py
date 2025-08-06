7cc97b06075cb569bdc6ef3d52bb32f94eac1230
Neha-meharin <nehamehrin09@gmail.com>
2025-08-06 11:09:43 +0530

    Create 4.py

---
+++ Day_09/4.py
@@ -0,0 +1,9 @@
+n=int(input("enter first number"))
+o=int(input("enter second number"))
+m=int(input("enter Third num"))
+if n==o and n==m:
+    print(n)
+if n==o and m!=n:
+    print(2)
+if n!=o and o!=m:
+    print(0)
